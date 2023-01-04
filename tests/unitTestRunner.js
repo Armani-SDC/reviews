7c2c62a4149ac0aabc9422b3c3ccf7180d76664d
sindwarf <sindorfsolutions@gmail.com>
2023-01-04 10:49:41 -0800

    working unit tests without checks

--- tests/unitTestRunner.js
+++ tests/unitTestRunner.js
@@ -1,19 +1,15 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 import getReviewTest from './unitTests/getReviewTest.js';
+import postReviewTest from './unitTests/postReviewTest.js';
 import metaTest from './unitTests/getMetaTest.js';
 import helpfulTest from './unitTests/helpfulTest.js';
 import reportTest from './unitTests/reportTest.js';
-
-// export let options = {
-//   noConnectionReuse: false,
-//   vus: 1,
-//   duration: '10s'
-// }
 
 export default () => {
   getReviewTest();
   metaTest();
   helpfulTest();
   reportTest();
+  postReviewTest();
 };
--- tests/unitTests/postReviewTest.js
+++ tests/unitTests/postReviewTest.js
@@ -9,6 +9,42 @@ import { sleep } from 'k6';
 
 export default () => {
   let randomProduct = Math.floor(Math.random() * (1000000 - 800000 + 1) + 800000);
-  http.get('http://localhost:3000/reviews?page=0&count=5&sort=newest&product_id=899333');
+  const url = 'http://localhost:3000/reviews/';
+  const payload = JSON.stringify(
+    {
+      product_id: 1,
+      rating: 2,
+      summary: 'this is a test summary',
+      body: 'this is a test body',
+      recommend: false,
+      name: 'test name',
+      email: 'testEmail@test.com',
+      photos: ['testurl1', 'testurl2'],
+      characteristics:
+      {
+          Fit: {
+              id: 1,
+              value: 4
+          },
+          Length: {
+              id: 2,
+              value: 3
+          },
+          Comfort: {
+              id: 3,
+              value: 5
+          },
+          Quality: {
+              id: 4,
+              value: 4
+          }
+      }
+  });
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  http.post(url, payload, params);
   sleep(1);
 };
