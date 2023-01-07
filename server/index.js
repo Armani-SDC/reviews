848961602c9377f01007c7ec9c813faf4d371b49
sindwarf <sindorfsolutions@gmail.com>
2023-01-07 10:56:16 -0800

    removed token

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const router = require('./router');
 
 const app = express();
 app.use(express.json());
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 // app.use(express.static(path.join(__dirname, '../client/dist')));
 
@@ -15,9 +15,5 @@ app.listen(PORT);
 app.use(express.json());
 
 app.use('/', router);
-
-app.get('/loaderio-446de1c19f6c7b5d6b4db29612425543', (req, res) => {
-  res.send('loaderio-446de1c19f6c7b5d6b4db29612425543');
-});
 
 console.log(`Listening at http://localhost:${PORT}`);
